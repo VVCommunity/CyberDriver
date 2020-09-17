bf7fd75c4fd08fd59df6666de261d1bc7e383f03
Maxim Podreshetnikov <maksim_podreshetnikov@outlook.com>
2020-09-17 20:12:06 +0700

    Add pause button and pause logic #U1-23

---
+++ Assets/Scripts/GUI/Buttons/ButtonStateHandler.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace GUI.Buttons
+{
+    public class ButtonStateHandler : MonoBehaviour
+    {
+        public Sprite FirstStateSprite;
+
+        public Sprite SecondStateSprite;
+
+        private Button button;
+
+        private void Awake()
+        {
+            button = GetComponentInParent<Button>();
+            if (button is null)
+            {
+                throw new MissingComponentException("Button state handler have to be attached to a button.");
+            }
+            button.onClick.AddListener(Click);
+        }
+
+        private void Click()
+        {
+            if (button.image.sprite == FirstStateSprite)
+            {
+                button.image.sprite = SecondStateSprite;
+            }
+            else
+            {
+                button.image.sprite = FirstStateSprite;
+            }
+        }
+    }
+}
