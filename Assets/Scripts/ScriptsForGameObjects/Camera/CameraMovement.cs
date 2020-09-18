fe6fdeb290e9d65937ba277ca3aafc061b6603ac
mi-los-dev <milos01dev@gmail.com>
2020-09-18 16:48:07 +0700

    U1-26-Refactoring
    Refactoring the base project

---
+++ Assets/Scripts/ScriptsForGameObjects/Camera/CameraMovement.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    [SerializeField]
+    private Car car;
+    [SerializeField]
+    private Vector3 offSet;
+
+    private void FixedUpdate()
+    {
+        transform.position = offSet + car.transform.position;
+    }
+}
